33385982874d9c95773391347226ff27b621c49d
TalBale <pirty43555@gmail.com>
2024-07-26 13:42:00 +0200

    final comit to the end 26.07.2024.13:42

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,28 @@
-let imagesBox = document.getElementsByClassName("images")[0];
-let imagesBottom = imagesBox.offsetTop + imagesBox.offsetHeight;
-let bottomY;
-window.onscroll = function () {
-  bottomY = window.scrollY + window.innerHeight;
-  if (
-    bottomY > imagesBox.offsetTop &&
-    window.scrollY < imagesBottom &&
-    imagesBox.classList.contains("transition") == false
-  ) {
-    console.log("box is riched");
-    imagesBox.classList.add("transition");
-  }
-};
+const burger = document.querySelector(".burger");
+burger.addEventListener("click", burgerAppear);
+function burgerAppear() {
+  const overlay = document.createElement("div");
+  const mobileMenu = document.createElement("div");
+
+  overlay.className = "overlay";
+
+  mobileMenu.innerHTML = `
+  <div class="container-nav-mobile">
+    <div><a href="/index.html">home</a></div>
+    <div><a href="/available-art.html">available art</a></div>
+    <div><a href="/about-me.html">about me</a></div>
+    <div><a href="contacts.html">contacts</a></div>
+  </div>
+  `;
+  mobileMenu.classList = "nav mobile";
+
+  document.body.appendChild(overlay);
+  document.body.appendChild(mobileMenu);
+  overlay.addEventListener("click", burgerDisappear);
+}
+function burgerDisappear() {
+  const overlay = document.querySelector(".overlay");
+  const mobileMenu = document.querySelector(".nav.mobile");
+  overlay.remove();
+  mobileMenu.remove();
+}
